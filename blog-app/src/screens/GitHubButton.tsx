6666fa4978a4dd4554b2178e3dc435a862940678
khiba-k <khibakoenane@gmail.com>
2024-12-20 13:34:53 +0200

    implemented github oAuth

---
+++ blog-app/src/screens/GitHubButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useSignInWithGithub } from "react-firebase-hooks/auth";
+import { FaGithub } from "react-icons/fa";
+import { auth } from "@/lib/firebaseConfig";
+import { useRouter } from "next/navigation";
+
+const GitHubButton: React.FC = () => {
+  const [signInWithGithub, githubUser, gitHubLoading, gitHubError] =
+    useSignInWithGithub(auth);
+  const router = useRouter();
+
+  // Sign In With Github
+  const handleGitHubSignIn = async () => {
+    try {
+      const result = await signInWithGithub();
+
+      if (result) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("Github error", error);
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <Button className="w-full" onClick={handleGitHubSignIn}>
+        <FaGithub style={{ marginRight: "10px", fontSize: "16px" }} /> Sign in
+        with Github
+      </Button>
+    </div>
+  );
+};
+export default GitHubButton;
